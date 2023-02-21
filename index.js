1d75cf2882bd872959a25d7ffa8401671b203496
Tshakalisa <tkhupe@gmail.com>
2023-02-21 00:20:23 -0600

    added methods

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const person = {
     name: ["Tshakalisa" , "Khupe"],
+
+    
     age: 40,
     bio: function () {
         console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
@@ -13,4 +15,27 @@ person.name;
 person.name[0];
 person.age;
 person.bio();
-person.introduceSelf()
+person.introduceSelf();
+
+
+// const person = {
+//     name: ["Bob", "Smith"],
+//     age: 32,
+//   };
+  
+  function logProperty(propertyName) {
+    console.log(person[propertyName]);
+  }
+  
+  logProperty("name");
+  // ["Bob", "Smith"]
+  logProperty("age");
+  // 32
+
+  person["eyes"] = "hazel";
+person.farewell = function () {
+  console.log("Bye everybody!");
+};
+
+person["eyes"];
+person.farewell();
